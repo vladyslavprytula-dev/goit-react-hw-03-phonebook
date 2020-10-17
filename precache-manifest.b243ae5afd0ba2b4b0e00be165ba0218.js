94d79bb5b880bcde35351214110d1ae11843d7db
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-10-17 14:53:29 +0300

    Updates

---
+++ precache-manifest.b243ae5afd0ba2b4b0e00be165ba0218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f7d5130af9ae22e6cf62575b82da95",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "84a14213447ed0c7af53",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.52f79d6e.chunk.css"
+  },
+  {
+    "revision": "608cf3b25947ac823b7c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.4c44fb1c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.4c44fb1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a14213447ed0c7af53",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.57a9a4e5.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
